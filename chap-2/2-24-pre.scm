cde9286f683338cd50e69b08d3e60e926133a455
waltermitty <waltermitty121906@gmail.com>
2021-02-03 06:59:42 +0800

    add: 2-25

---
+++ chap-2/2-24-pre.scm
@@ -0,0 +1,13 @@
+(define (length items)
+    (if (null? items)
+        0
+        (+ 1 (length (cdr items)))))
+
+(define (count-leaves x)
+    (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                    (count-leaves (cdr x))))))
+(define x (cons (list 1 2) (list 3 4)))
+
+(display (count-leaves x))
---
+++ chap-2/2-25.scm
@@ -0,0 +1,12 @@
+(define items (list 1 3 (list 5 7) 9))
+(display items)
+(newline)
+(display (cadr (caddr items)))
+; (display (car (caddr items)))
+(newline)
+(define items-2 (list (list 7)))
+(display (car (car items-2)))
+(newline)
+
+(define items-3 (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+(display (cadadr (cadadr (cadadr items-3))))
