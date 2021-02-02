54ee4aea7092e9a27e854a72bfad4f28ca7d6a98
waltermitty <waltermitty121906@gmail.com>
2021-02-03 07:14:36 +0800

    add: 2-26

---
+++ chap-2/2-26.scm
@@ -0,0 +1,18 @@
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(define test_1 (append x y))    ; (1 2 3 4 5 6)
+(define test_2 (cons x y))      ; ((1 2 3) (4 5 6))
+(define test_3 (list x y))      ;((1 2 3) (4 5 6))
+
+(display test_1)
+(newline)
+(display test_2)
+(newline)
+(display test_3)
+(newline)
+(display (cons y x))
+(newline)
+(display (cdr test_2))
+(newline)
+(display (car test_2))
