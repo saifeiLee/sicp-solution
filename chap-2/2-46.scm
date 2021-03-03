fa203a8f0f9694d65739082389da942c9024e3cf
waltermitty <waltermitty121906@gmail.com>
2021-03-04 07:19:55 +0800

    add: 2-46

---
+++ chap-2/2-46.scm
@@ -0,0 +1,27 @@
+(define (make-vect x y)
+    (cons x y))
+
+(define (xcor-vect vect)
+    (car vect))
+
+(define (ycor-vect vect)
+    (cdr vect))
+
+(define (add-vect vec1 vec2)
+    (let ((x1 (xcor-vect vec1))
+            (y1 (ycor-vect vec1))
+            (x2 (xcor-vect vec2))
+            (y2 (ycor-vect vec2)))
+        (make-vert (+ x1 x2)
+                    (+ y1 y2))))
+
+(define (sub-vect vec1 vec2)
+    (make-vect (- (xcor-vect vec1)
+                  (xcor-vect vec2))
+               (- (ycor-vect vec1)
+                  (ycor-vect vec2))))
+
+((define (scale-vect s vect)
+    (make-vect (* s (xcor-vect vect))
+                (* s (ycor-vect vect)))))
+; test 
