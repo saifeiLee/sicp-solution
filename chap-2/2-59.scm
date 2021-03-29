b7397c872a7523071cc99cf0174e85b8ce8b69ea
waltermitty <waltermitty121906@gmail.com>
2021-03-30 06:52:39 +0800

    add: 2-59

---
+++ chap-2/2-59.scm
@@ -0,0 +1,31 @@
+(define (element-of-set? x set)
+    (cond ((null? set) false)
+            ((equal? x (car set)) true)
+            (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+    (if (element-of-set? x set)
+        set
+        (cons x set)))
+
+(define (intersection-set set1 set2)
+    (cond ((or (null? set1) (null? set2)) '())
+            ((element-of-set? (car set1) set2)
+                (cons (car set1) (intersection-set (cdr set1) set2)))
+            (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+    (let ((inter (intersection-set set1 set2)))
+        (append set2
+            (filter 
+              (lambda (x)
+                (not (element-of-set? x inter)))
+            set1))))
+
+(define (print item)
+    (display item)
+    (newline))
+; test
+(define set1 (list 1 2 3))
+(define set2 (list 4 5 6))
+(print (union-set set1 set2))
